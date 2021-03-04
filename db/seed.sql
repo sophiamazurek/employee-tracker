137738f68552a135eb5205c83fc1170b4a01ef96
Sophia Mazurek <sophiamazurek@Sophias-MacBook-Pro-2.local>
2021-03-04 15:28:24 -0600

    added seed.sql, needs to ne looked over

--- db/seed.sql
+++ db/seed.sql
@@ -8,3 +8,9 @@ VALUES (2, "Marketing");
 
 INSERT INTO employeerole (id, title, salary, department_id)
 VALUES (1, "Social Media Coord",20000,2 );
+
+INSERT INTO employeerole (id, title, salary, department_id)
+VALUES (2, "Developer",30000,3 );
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (1, "Sophia", "Mazurek", Manager, )
